29531ee16a2db9aacf12a8d01c854463aa78eba6
QuangHuy193 <quanghuylhpq@gmail.com>
2026-09-24 14:21:14 +0700

    fiexd: lỗi chữ hoa thường

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useToggleStore } from "@/lib/useStore/useToggleStore";
-import MainTabsBar from "@/app/components/navbar/mainTabsBar";
+import MainTabsBar from "@/app/components/navbar/tempmainTabsBar";
 import { RENDER_CONTENT } from "@/lib/constants/tsxConstants";
 import { useEffect } from "react";
 import { init } from "@/lib/services";
